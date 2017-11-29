5057313ab01c46b0c80fa66f2c2c3e73f0746585
Vigneshwaran Ramanathan <37493@hexaware.com>
2017-11-29 16:14:34 +0530

    Initial code for sql server integration with node

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   
+    var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        user: 'Corp\37493',
+        password: 'VIgnesh_234',
+        server: 'localhost', 
+        database: 'devPractice' 
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('select * from tblEmployees', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+});
+
+var server = app.listen(5000, function () {
+    console.log('Server is running..');
+});
